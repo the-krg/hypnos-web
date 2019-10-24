96fb963c72ece7558fb6037dbd0f72cdcfb77de8
Enzo Haguiwara <haguiwara68@gmail.com>
2019-10-24 02:21:11 -0300

    Add Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
+  devise_scope :user do
+    unauthenticated :user do
+      root to: 'users/sessions#new'
+    end
+
+    authenticated :user do
+      root to: 'dashboard#index', as: :authenticated_root
+    end
+  end
 end
