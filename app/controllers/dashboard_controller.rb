6599b93a063f97f3c53dd90e456cb5f75f869a79
Enzo Haguiwara <haguiwara68@gmail.com>
2019-11-13 02:02:08 -0300

    Add more statistics to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,25 +3,45 @@ class DashboardController < ApplicationController
     labels = []
     last_week_nights = []
     last_week_days = []
-    average = []
+    day_quality = []
+    night_quality = []
+    average_quality = []
+    sleep_time = []
+    nights = current_user.nights
+    days = current_user.days
 
-    (6.days.ago.to_date..Date.today).map do |day|
-      today = (day.beginning_of_day..day.end_of_day)
+    (6.days.ago.to_date..Date.today).map do |n_day|
+      today = (n_day.beginning_of_day..n_day.end_of_day)
 
-      night_quality = (current_user.nights.find_by(created_at: today) || { quality: 0 })
-      day_quality = (current_user.days.find_by(created_at: today) || { quality: 0 })
+      night = (nights.find_by(created_at: today) || empty_day)
+      day = (days.find_by(created_at: today) || empty_day)
 
-      last_week_nights << night_quality
-      last_week_days << day_quality
-      average << ((night_quality[:quality] + day_quality[:quality]) / 2.0)
-      labels << day.strftime('%a')
+      sleep_time << ((night[:wake_hour] - night[:sleep_hour]).to_i/3600).round(2)
+
+      last_week_nights << night
+      last_week_days << day
+      day_quality << day[:quality]
+      night_quality << night[:quality]
+      average_quality << ((night[:quality] + day[:quality]) / 2.0)
+      labels << n_day.strftime('%a')
     end
 
     @json = {
       labels: labels,
       nights: last_week_nights.pluck(:quality),
       days: last_week_days.pluck(:quality),
-      average: average
+      sleep_time: sleep_time,
+      day_quality: day_quality,
+      night_quality: night_quality,
+      average_quality: average_quality
+    }
+  end
+
+  def empty_day
+    {
+      quality: 0,
+      sleep_hour: 0,
+      wake_hour: 0,
     }
   end
 
