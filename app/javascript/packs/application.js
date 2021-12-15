b15bc2522de4ba87fab0e371977fa191f773ea01
Enzo Haguiwara <haguiwara68@gmail.com>
2021-12-15 02:07:05 -0300

    update chart js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,12 @@ require('@rails/activestorage').start();
 require('channels');
 require('materialize-css');
 require('jquery');
-require('chart.js');
+import Chart from 'chart.js/auto';
+import AnnotationPlugin from 'chartjs-plugin-annotation';
+
+Chart.register(AnnotationPlugin);
+global.Chart = Chart;
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
